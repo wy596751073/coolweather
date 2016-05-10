8e8fe8218ccbc38577846158a77331bb9f55063d
wangyu <596751073@qq.com>
2016-05-10 23:10:53 +0800

    First commit.

---
+++ src/com/coolweather/app/CoolWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.coolweather.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoolWeatherActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
